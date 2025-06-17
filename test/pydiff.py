3a86588a585c141908328c52c3a708d451209cd2
CK Tan <cktanx@gmail.com>
2025-06-17 11:56:01 -0700

    save

--- test/pydiff.py
+++ test/pydiff.py
@@ -1,10 +1,16 @@
 import sys
 
-with open(sys.argv[1]) as f1:
-    f1 = eval(f1.read())
+try:
+    with open(sys.argv[1]) as f1:
+        f1 = eval(f1.read())
+except FileNotFoundError:
+    sys.exit(f"File not found: {sys.argv[1]}")
 
-with open(sys.argv[2]) as f2:
-    f2 = eval(f2.read())
+try:
+    with open(sys.argv[2]) as f2:
+        f2 = eval(f2.read())
+except FileNotFoundError:
+    sys.exit(f"File not found: {sys.argv[2]}")
 
 if f1 != f2:
     sys.exit(1)
